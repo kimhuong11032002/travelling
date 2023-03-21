eb6e0c38cff97c2510a6f546fd2db54724efa062
Kim huong <vn.kimhuong@gmail.com>
2023-03-21 21:26:53 +0700

    them 1 vi du

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
+ print("I am adding example.py file to the remote repository")
+
+
+
 def print_name(name):
  print("name is: {}".format(name))
+
+print_name("sinh")
