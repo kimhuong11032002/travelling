8e3c2b7b4b46274f1388dbad9aff7ed9ae2fec78
Kim huong <vn.kimhuong@gmail.com>
2023-03-21 21:41:26 +0700

    them ket qua khi in

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository")
 
-
-
 def print_name(name):
  print("name is: {}".format(name))
 
-print_name("sinh")
+print_name("sinh") ## => name is: sinh
+print_name('sang')
+print_name('tai')
